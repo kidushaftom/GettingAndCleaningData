1846f70cd01ea99ed0c60ee976739e3f00c116d8
haftom <haftomermias@gmail.com>
2018-09-16 16:37:06 -0700

    Getting and Cleaning Data

--- run_analysis.R
+++ run_analysis.R
@@ -5,7 +5,7 @@
 #Appropriately labels the data set with descriptive variable names.
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-#________________________________________________________________________________________________________________
+#_______________Haftom Gebrekristos_________________________________________________________________________________________________
 
 setwd("C:/Users/Haftom/GettingAndCleaningData")
 dir<-getwd()
